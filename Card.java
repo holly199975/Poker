434514e74cee1face11395822c945229d3ec7a5c
Holly Deng <holly199975@gmail.com>
2018-12-11 03:22:22 +0800

    Initial commit

---
+++ Card.java
@@ -0,0 +1,13 @@
+package com.holly.poker;
+
+public class Card {
+    char[] suits = {'\u2663', '\u2666', '\u2665', '\u2660'};
+    int value;
+    public Card(int value) {
+        this.value = value;
+    }
+
+    public String get() {
+        return value%13+1 + "" + suits[value/13];
+    }
+}
---
+++ Poker.java
@@ -0,0 +1,20 @@
+package com.holly.poker;
+import java.util.Random;
+
+public class Poker  {
+    Card[] cards = new Card[52];
+    public Poker () {
+        for (int i=0; i<52; i++) {
+            cards[i] = new Card(i);
+        }
+    }
+
+    public void shuffle(){
+        for (int i=0; i<52; i++) {
+            int r = new Random().nextInt(52);
+            Card tmp = cards[i];
+            cards[i] = cards[r];
+            cards[r] = tmp;
+        }
+    }
+}
